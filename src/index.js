f6dc4d96b17870207dc1c9d960f1f38c9f0c2e68
ceyhun-bitech <ceyhun.atagec@bitechnology.com>
2023-01-16 16:53:19 +0300

    ui revision

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
-import { AuthType } from '@qlik/sdk';
-import embed from './configure';
-import connect from './connect';
+import { AuthType } from "@qlik/sdk";
+import embed from "./configure";
+import connect from "./connect";
 
-var outputArea = $("#chat-output");
+var outputArea = $("#msger-chat");
 /* eslint-disable */
 import barchart from "@nebula.js/sn-bar-chart";
 import { embed } from "@nebula.js/stardust";
@@ -22,7 +22,7 @@ const charts = { barchart };
 //     webIntegrationId: 'wXcLRgyI2s_94ZX7uOGSSnfk-ta14WSm',
 //   });
 
-//   // İstemci Kimliği: c461b5e68cda7bbaa50c4c2d3d116dfa, 
+//   // İstemci Kimliği: c461b5e68cda7bbaa50c4c2d3d116dfa,
 //   // İstemci parolası: e65903d1d80f9ceae1466a8b538c6e9d2f7a40a2226d7fd34edebf40e02e767c
 
 //   const n = embed(app);
@@ -34,52 +34,84 @@ const charts = { barchart };
 
 // run();
 
+const messageForm = $(".msger-inputarea");
+const messageInput = $(".msger-input");
+const messageChat = $(".msger-chat");
 
-var outputArea = $("#chat-output");
+// Icons made by Freepik from www.flaticon.com
+const BOT_NAME = "Bot";
+const PERSON_NAME = "You";
 
-$("#user-input-form").on("submit", function (e) {
+messageForm.on("submit", function (e) {
   e.preventDefault();
-  var message = $("#user-input").val();
-  outputArea.append(`
-    <div class='bot-message'>
-      <div class='message'>
-        ${message}
-      </div>
-    </div>
-  `);
-  console.log('send question')
+  var message = messageInput.val();
+  if (!message) return;
+  // outputArea.append(`
+  //   <div class=''>
+  //     <div class='message'>
+  //       ${message}
+  //     </div>
+  //   </div>
+  // `);
+
+  appendMessage(PERSON_NAME, "right", message);
+
+  console.log("send question");
   sendQuestion(message); // to send NL API request and render response
-  $("#user-input").val("");
+  messageInput.val("");
 });
 
+function appendMessage(name, side, text) {
+  //   Simple solution for small apps
+  const msgHTML = `
+    <div class="msg ${side}-msg">
 
-
-$("#speak").on("click", function (e) { 
-  const speak = document.getElementById('speak');
-  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-  const recognition = new SpeechRecognition();
-  recognition.lang = "en-US",
-
-  // speak.addEventListener('click', function() {
-      recognition.start();
-  // }) 
-  
-  recognition.onresult = function(e) {
-  console.log('e',e)
-      const message = e.results[0][0].transcript;
-      console.log(message);
-      outputArea.append(`
-      <div class='bot-message'>
-        <div class='message'>
-          ${message}
+      <div class="msg-bubble">
+        <div class="msg-info">
+          <div class="msg-info-name">${name}</div>
+          <div class="msg-info-time">${formatDate(new Date())}</div>
         </div>
+
+        <div class="msg-text">${text}</div>
       </div>
-    `);
-      sendQuestion(message)
-  }
-})
+    </div>
+  `;
 
+  messageChat.append(msgHTML);
+  messageChat.scrollTop += 500;
+}
 
+function formatDate(date) {
+  const h = "0" + date.getHours();
+  const m = "0" + date.getMinutes();
+
+  return `${h.slice(-2)}:${m.slice(-2)}`;
+}
+
+$("#speak").on("click", function (e) {
+  const speak = document.getElementById("speak");
+  const SpeechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
+  (recognition.lang = "en-US"),
+    // speak.addEventListener('click', function() {
+    recognition.start();
+  // })
+
+  recognition.onresult = function (e) {
+    const message = e.results[0][0].transcript;
+    // outputArea.append(`
+    //   <div class='bot-message'>
+    //     <div class='message'>
+    //       ${message}
+    //     </div>
+    //   </div>
+    // `);
+    appendMessage(PERSON_NAME, "right", message);
+
+    sendQuestion(message);
+  };
+});
 
 // const requestUrl = 'https://08uu6507zw4v6cu.eu.qlikcloud.com';
 // const data = JSON.stringify({
@@ -98,56 +130,58 @@ $("#speak").on("click", function (e) {
 //   body: data,
 // });
 
-
 async function sendQuestion(message) {
-  console.log('require', require)
-  const requestUrl = 'https://08uu6507zw4v6cu.eu.qlikcloud.com';
-const data = JSON.stringify({
-  text: message,
-  app: { id: "b139e5ee-7adc-41e5-8c1f-0b7be463729f", name: "CHAT" },
-  enableVisualizations: true,
-  visualizationTypes: ["barchart"],
-});
-const response = await fetch(`${requestUrl}/api/v1/questions/actions/ask`, {
-  method: "POST",
-  headers: {
-    Authorization: "Bearer eyJhbGciOiJFUzM4NCIsImtpZCI6ImQyMmJhYTc0LTM1YmEtNGM5Mi1hZjEzLTJkMGM2ODM4YzhhMiIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiWDZuZXl4WTNncEtOM21WZTZIdzhLX0ltMXh5UzBSZkQiLCJqdGkiOiJkMjJiYWE3NC0zNWJhLTRjOTItYWYxMy0yZDBjNjgzOGM4YTIiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiaUF6eUxZRk9Wd1VZV2cwX0ZtUWROOFk0azMzZGZMVVgifQ.gPEhRnfmMlriUPDHj-Fsitcqspn72gjGc2vxTv_G3PkmX5MDf3JQfGjNsN-xNDDtozF-IaLVQ3ZrIMYQyGcaoX4f03uSl9plsJIqxGWEe3UzVC6E-40VyKO0ttSEvPPl",
-    "Content-Type": "application/json",
-    "qlik-web-integration-id": "wXcLRgyI2s_94ZX7uOGSSnfk-ta14WSm",
-  },
-  body: data,
-});
+  const requestUrl = "https://08uu6507zw4v6cu.eu.qlikcloud.com";
+  const data = JSON.stringify({
+    text: message,
+    app: { id: "b139e5ee-7adc-41e5-8c1f-0b7be463729f", name: "CHAT" },
+    enableVisualizations: true,
+    visualizationTypes: ["barchart"],
+  });
+  const response = await fetch(`${requestUrl}/api/v1/questions/actions/ask`, {
+    method: "POST",
+    headers: {
+      Authorization:
+        "Bearer eyJhbGciOiJFUzM4NCIsImtpZCI6ImQyMmJhYTc0LTM1YmEtNGM5Mi1hZjEzLTJkMGM2ODM4YzhhMiIsInR5cCI6IkpXVCJ9.eyJzdWJUeXBlIjoidXNlciIsInRlbmFudElkIjoiWDZuZXl4WTNncEtOM21WZTZIdzhLX0ltMXh5UzBSZkQiLCJqdGkiOiJkMjJiYWE3NC0zNWJhLTRjOTItYWYxMy0yZDBjNjgzOGM4YTIiLCJhdWQiOiJxbGlrLmFwaSIsImlzcyI6InFsaWsuYXBpL2FwaS1rZXlzIiwic3ViIjoiaUF6eUxZRk9Wd1VZV2cwX0ZtUWROOFk0azMzZGZMVVgifQ.gPEhRnfmMlriUPDHj-Fsitcqspn72gjGc2vxTv_G3PkmX5MDf3JQfGjNsN-xNDDtozF-IaLVQ3ZrIMYQyGcaoX4f03uSl9plsJIqxGWEe3UzVC6E-40VyKO0ttSEvPPl",
+      "Content-Type": "application/json",
+      "qlik-web-integration-id": "wXcLRgyI2s_94ZX7uOGSSnfk-ta14WSm",
+    },
+    body: data,
+  });
   const brokerResponse = await response.json();
   let properties;
   let lang = "en-US";
 
-  if ("narrative" in brokerResponse.conversationalResponse.responses[0]) {
-    const temp =
-      brokerResponse.conversationalResponse.responses[0].narrative.text;
-    outputArea.append(`
-  <div class='user-message'>
-    <div class='message'>
-      ${temp}
-    </div>
-  </div>
-`);
+  const chatResponse = brokerResponse.conversationalResponse.responses;
+
+  if (!chatResponse.length) return  appendMessage(BOT_NAME, "left", "Please try a different query.");
+
+  if ("narrative" in chatResponse[0]) {
+    const msgText =
+      chatResponse[0].narrative.text;
+    //   outputArea.append(`
+    // <div class='user-message'>
+    //   <div class='message'>
+    //     ${temp}
+    //   </div>
+    // </div>`);
+
+    appendMessage(BOT_NAME, "left", msgText);
   } else if (
-    "imageUrl" in brokerResponse.conversationalResponse.responses[0] ||
-    "renderVisualization" in brokerResponse.conversationalResponse.responses[0]
+    "imageUrl" in chatResponse[0] ||
+    "renderVisualization" in chatResponse[0]
   ) {
     let chartElement, img;
     const nebulaChartId = `nebula-chart-${new Date().getTime()}`;
-    const nebulaObject = brokerResponse.conversationalResponse.responses.filter(
+    const nebulaObject = chatResponse.filter(
       (x) => x.type === "nebula"
     );
-    const imgUrlObject = brokerResponse.conversationalResponse.responses.filter(
+    const imgUrlObject = chatResponse.filter(
       (x) => x.type === "chart"
     );
     if (nebulaObject.length) {
-      console.log('nebulaObject', nebulaObject)
       properties = { ...nebulaObject[0].renderVisualization.data };
       properties.color.paletteColor.index = 3;
-      console.log('properties', properties)
       lang = nebulaObject[0].renderVisualization.language;
       chartElement = `<div class='user-message'>
             <div class='message'>
@@ -158,37 +192,67 @@ const response = await fetch(`${requestUrl}/api/v1/questions/actions/ask`, {
       img = imgUrlObject[0].imageUrl;
       chartElement = `<a href="https://08uu6507zw4v6cu.eu.qlikcloud.com/${img}"><img src="https://08uu6507zw4v6cu.eu.qlikcloud.com/${img}" width="600" height="600 "></a>`;
     }
-    if ("narrative" in brokerResponse.conversationalResponse.responses[1]) {
+    if ("narrative" in chatResponse[1]) {
       const text_r =
-        brokerResponse.conversationalResponse.responses[1].narrative.text;
-      outputArea.append(`
+        chatResponse[1].narrative.text;
+      const chartHTML = `
       <div class='user-message'>
       <div class ="message">
       ${text_r} </br>
       ${chartElement}
       </div>
       </div>
-    `);
+    `;
+      //   outputArea.append(`
+      //   <div class='user-message'>
+      //   <div class ="message">
+      //   ${text_r} </br>
+      //   ${chartElement}
+      //   </div>
+      //   </div>
+      // `);
+
+      appendMessage(BOT_NAME, "left", chartHTML, true);
       if (nebulaObject.length) render(properties, nebulaChartId, lang);
-    } else if ("nebula" in brokerResponse.conversationalResponse.responses[0]) {
-      if (brokerResponse.conversationalResponse.responses) {
-        outputArea.append(`
-          <div class='user-message'>
+    } else if ("nebula" in chatResponse[0]) {
+      if (chatResponse) {
+        const chartHTML = `
+           <div class='user-message'>
               <div class='message'>
-                <div class='nebula-chart'  id="${nebulaChartId}"></div>
-              </div>
-          </div>
-        `);
+                 <div class='nebula-chart'  id="${nebulaChartId}"></div>
+               </div>
+           </div>
+         `;
+        // outputArea.append(`
+        //   <div class='user-message'>
+        //       <div class='message'>
+        //         <div class='nebula-chart'  id="${nebulaChartId}"></div>
+        //       </div>
+        //   </div>
+        // `);
+
+        appendMessage(BOT_NAME, "left", chartHTML, true);
         render(properties, nebulaChartId, lang);
+
+        // appendMessage(BOT_NAME, BOT_IMG, "left", chartHTML, isChart);
       }
     } else {
-      outputArea.append(`
+      const chartHTML = `
       <div class='user-message'>
         <div class='message'>
         <img src="https://08uu6507zw4v6cu.eu.qlikcloud.com/${img}" width="300" height="200">
         </div>
       </div>
-    `);
+    `
+    //   outputArea.append(`
+    //   <div class='user-message'>
+    //     <div class='message'>
+    //     <img src="https://08uu6507zw4v6cu.eu.qlikcloud.com/${img}" width="300" height="200">
+    //     </div>
+    //   </div>
+    // `);
+
+    appendMessage(BOT_NAME, "left", chartHTML, true);
     }
   }
 }
